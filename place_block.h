1a30bb97ba38eb00ad3edc9d0efe4afe14e3394b
ccucsiehyse <119291223+ccucsiehyse@users.noreply.github.com>
2023-05-28 23:32:44 +0800

    Add files via upload

---
+++ place_block.h
@@ -0,0 +1,7 @@
+void map_init(); //map initialize
+void FO_map(); //file output map
+void block_place_del(); //place or delete block at cursor
+void FI_map(); //file input map
+//void cursor_move(); //(may have better method)
+
+short xy_map[2], xy_4array[4][2]; //record absolute xy coordinate
