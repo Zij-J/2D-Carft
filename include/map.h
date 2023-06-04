304aef341e72fa47f95cda684d7799e1e9b0b5a2
ccucsiehyse <119291223+ccucsiehyse@users.noreply.github.com>
2023-06-04 13:45:38 +0800

    Update map.h

--- include/map.h
+++ include/map.h
@@ -2,16 +2,35 @@
 #ifndef MAP
 #define MAP
 
+#include "../include/basicSetting.h"
+#define TOTAL_BLOCK_NUMBER_IN_WIDTH 18
+#define TOTAL_BLOCK_NUMBER_IN_HEIGHT (TOTAL_BLOCK_NUMBER_IN_WIDTH * WINDOW_WIDTH / WINDOW_HEIGHT)
+#define BLOCK_WIDTH (WINDOW_WIDTH / TOTAL_BLOCK_NUMBER_IN_WIDTH) // 用寬能放幾個推算方塊的邊長
+#define ARRAY_MAP_WIDTH 30
+
+extern short map[4][ARRAY_MAP_WIDTH][ARRAY_MAP_WIDTH]; //4 big array
+extern SDL_position camera_xy;//map_UpLeft_abs_pos
+extern SDL_position arr_xy[4];//big_arr_abs_pos
+extern SDL_position cursor_xy;//cursor_abs_pos
+
 // unfinished
-void Maps_Init();
-void Maps_Clear();
-bool Map_isInput(SDL_Event *event);
-int ***Map_GetTotalMap();
-SDL_position Map_GetUpLeftCornerPosition();
-SDL_position Map_GetCursorPosition();
-SDL_size Map_GetBlockSize();
-SDL_size Map_GetCursorSize();
+
+//bool Map_isInput(SDL_Event event);//not need now //merge in Map_EditBlock()
+//short **Map_GetTotalMap(); //altered by using extern array
 
 // finished
+
+void Map_Init();
+private void Map_Finput(FILE *fp, int n, int x, int y);
+void Map_Clear();
+void Map_UpdateMaps(SDL_Event event);
+void swap(int* a, int* b);
+private void Map_Foutput(FILE *fp, int n, char* FileName);
+void Map_EditBlock(SDL_Event event);//need to get Hotbar current block number
+SDL_position Map_GetUpLeftCornerPosition(SDL_Event event);
+SDL_position Map_GetCursorPosition(SDL_Event event);
+SDL_size Map_GetBlockSize();
+SDL_size Map_GetCursorSize();
+private char* file_name(int x, int y);
 
 #endif
