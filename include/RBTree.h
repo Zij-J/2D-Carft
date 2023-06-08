997ceb57978253524367f6a2df9704150956235e
WeiHong89 <82870560+WeiHong89@users.noreply.github.com>
2023-06-08 14:26:22 +0800

    Create RBTree.h

---
+++ include/RBTree.h
@@ -0,0 +1,9 @@
+#ifndef RBTREE
+#define RBTREE
+
+void leftRotate();// 左旋轉
+void rightRotate();// 右旋轉
+void fixInsert();// 修復RBTree after insertion
+void insertNode();// 插入節點
+void treeToArray();// 將RBTree 轉換成陣列
+void sortArray();// 使用RBTree排序
