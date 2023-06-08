a09851bbeff18777f1d75d1423d1b83c91bf2527
WeiHong89 <82870560+WeiHong89@users.noreply.github.com>
2023-06-08 14:25:12 +0800

    Update texture.h(RBTree)

--- include/texture.h
+++ include/texture.h
@@ -15,6 +15,6 @@ short TextureBase_GetSearchedBlockID();// 把搜尋到的方塊編號回傳
 SDL_Texture *loadTexture();// 載入圖片資料夾圖片
 SDL_Texture *TextureBase_GetTextureName();// 依方塊名稱取得圖片
 SDL_Texture *TextureBase_GetTextureByID();// 依編號取得圖片
-SDL_Texture *TextureBase_GetTextureID();//依照方塊編號大小排序資料庫
+SDL_Texture *TextureBase_GetTextureID();//依照方塊編號大小排序資料庫(使用RBTree)
 
 #endif
