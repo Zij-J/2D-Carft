f264b7c368a0803f252de57645d8715382aa395d
WeiHong89 <82870560+WeiHong89@users.noreply.github.com>
2023-06-11 22:52:18 +0800

    Add files via upload

--- include/RBTree.h
+++ include/RBTree.h
@@ -1,9 +1,15 @@
-#ifndef RBTREE
-#define RBTREE
+#ifndef RBTree
+#define RBTree
 
-void leftRotate();// 左旋轉
-void rightRotate();// 右旋轉
-void fixInsert();// 修復RBTree after insertion
-void insertNode();// 插入節點
-void treeToArray();// 將RBTree 轉換成陣列
-void sortArray();// 使用RBTree排序
+#define P printf("*file: %s, func: %s, line: %d\n", __FILE__, __func__, __LINE__)
+
+typedef enum {BLACK, RED} COLOR;//node color
+typedef enum {NONE, LEFT, RIGHT} SIDE;//parent's L or R child
+
+struct tNode* RBT_init();//initiallize
+void insert(struct tNode *, blockBase_Data*);//insert the node to red black tree
+struct tNode* find(struct tNode*, blockBase_Data*);//find node by block's name
+struct tNode* find_root(struct tNode *);//find red black tree root
+int* store_data(struct tNode *);//store blockID to array
+
+#endif
